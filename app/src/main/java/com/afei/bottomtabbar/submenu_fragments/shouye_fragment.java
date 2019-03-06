c12ff9f1f85b338d1ab39bd149498f62e5509ee7
2541747035@qq.com <wjl984296155@#>
2019-03-06 17:58:34 +0800

    second

--- app/src/main/java/com/afei/bottomtabbar/submenu_fragments/shouye_fragment.java
+++ app/src/main/java/com/afei/bottomtabbar/submenu_fragments/shouye_fragment.java
@@ -75,17 +75,4 @@ public class shouye_fragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shouye_fragment, container, false);
     }
-
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-
 }
