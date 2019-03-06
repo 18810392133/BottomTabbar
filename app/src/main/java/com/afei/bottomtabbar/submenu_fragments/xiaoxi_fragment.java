2f5dc1e5a6ce3f47af7352615b294d4876da1d8a
2541747035@qq.com <wjl984296155@#>
2019-03-06 19:20:51 +0800

    second

--- app/src/main/java/com/afei/bottomtabbar/submenu_fragments/xiaoxi_fragment.java
+++ app/src/main/java/com/afei/bottomtabbar/submenu_fragments/xiaoxi_fragment.java
@@ -17,7 +17,7 @@ import com.afei.bottomtabbar.R;
  * Use the {@link xiaoxi_fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class xiaoxi_fragment extends Fragment {
+public class shequ_fragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
