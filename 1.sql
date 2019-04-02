dcc4313c8db81e1abe7285a4ec616a8db7fc8bfb
韦军林 <34645790+18810392133@users.noreply.github.com>
2019-04-02 18:22:53 +0800

    Add files via upload

--- 1.sql
+++ 1.sql
@@ -353,11 +353,11 @@ CREATE TABLE `user_info_writing` (
   `created` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_info_writing_created_4d16ee84` (`created`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `user_info_writing` */
 
-insert  into `user_info_writing`(`id`,`writing_name`,`writing_url`,`created`) values (1,'百乐','http://www.baidu.com','2019-04-02');
+insert  into `user_info_writing`(`id`,`writing_name`,`writing_url`,`created`) values (1,'百乐','http://www.baidu.com','2019-04-02'),(2,'weijnnlin','www.weijunlin.com','2019-04-18');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
